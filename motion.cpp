2bbff86f444d5108ed31ed627e7f3ced03c22e85
DanielWisn <146425555+DanielWisn@users.noreply.github.com>
2025-02-01 18:25:07 +0100

    motion-sensor

--- motion.cpp
+++ motion.cpp
@@ -2,9 +2,9 @@
 
 const int sensorPin = 2;
 const int ledPin = 13;
+
 int previousState = LOW;
 int currentState = LOW;
-
 
 void setup(){
   pinMode(sensorPin,INPUT);
@@ -15,9 +15,9 @@ void setup(){
 void loop(){
   previousState = currentState;
   currentState = digitalRead(sensorPin);
-
-  if (previousState == HIGH and currentState == LOW){
-    Serial.println("Sound detected");
+  
+  if (previousState == LOW and currentState == HIGH){
+    Serial.println("Movement");
     digitalWrite(ledPin,HIGH);
     delay(500);
     digitalWrite(ledPin,LOW);
