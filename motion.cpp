f0087815441c9ef39444f692051fb956c17977a6
DanielWisn <146425555+DanielWisn@users.noreply.github.com>
2025-02-01 17:16:50 +0100

    motion-sensor

---
+++ motion.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+const int sensorPin = 2;
+const int ledPin = 13;
+int previousState = LOW;
+int currentState = LOW;
+
+
+void setup(){
+  pinMode(sensorPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop(){
+  previousState = currentState;
+  currentState = digitalRead(sensorPin);
+
+  if (previousState == LOW and currentState == HIGH){
+    Serial.println("Ruch");
+    digitalWrite(ledPin,HIGH);
+    delay(500);
+    digitalWrite(ledPin,LOW);
+  }
+}
